f5abf98902f7685256cc61140e6151c125a36396
kjh6536 <114335024+kjh6536@users.noreply.github.com>
2022-09-26 21:37:50 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pen = 1200
+note = 2000
+delivery = 3000
+Tot_price = pen * 5 + note * 8 + delivery
+print("결제 금액 :", Tot_price, "원")
+
+
+# In[ ]:
+
+
+
+
