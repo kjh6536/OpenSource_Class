252cd01ae545e5e41de71cbaf9a10cff54dcd92c
kjh6536 <114335024+kjh6536@users.noreply.github.com>
2022-10-03 17:12:26 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
-#!/usr/bin/env python
-# coding: utf-8
+def findMax(a, b, c):
+    if a>b:
+         biggist = a
+    else:
+         biggist = b
+    
+    if c>biggist:
+        biggist = c
 
-# In[1]:
+  return biggist
 
+a = int(input("첫 번째 숫자 입력: "))
+b = int(input("두 번째 숫자 입력: "))
+c = int(input("세 번째 숫자 입력: "))
 
-pen = 1200
-note = 2000
-delivery = 3000
-Tot_price = pen * 5 + note * 8 + delivery
-print("결제 금액 :", Tot_price, "원")
+biggest = findMax(a, b, c)
 
-
-# In[ ]:
-
-
-
-
+print(a, b, c, "중 가장 큰 수는", biggest, "입니다.")
